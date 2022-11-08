f1cf81645073f67ab7aa2961f90883efd5415c3e
Your Name <you@example.com>
2022-11-08 17:21:23 +0530

    frontend backend setup

---
+++ config/dbConfig.js
@@ -0,0 +1,15 @@
+const mongoose=require("mongoose")
+
+mongoose.connect(process.env.mongo_url)
+
+const connection = mongoose.connection
+
+connection.on('connection',()=>{
+    console.log('Mongo DB Connection Successfull')
+})
+
+connection.on('error',()=>{
+    console.log('mongo DB Connection failed')
+})
+
+module.exports = mongoose;
