6ffb7406c3563a392146832d0999d13c22cdc3b0
Your Name <you@example.com>
2022-12-11 08:38:49 +0530

    FrontEnd for Result completed

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -14,11 +14,13 @@ function DefaultLayout(props) {
             <b className='secondary-text'>Computer Science Department </b>   
             Results {" "}
             </h1>
+            <div className='d-flex  align-items-center justify-content-center'>
             <h1 className='text-white text-medium'>{employee?.name}</h1>
             <h1 className='text-white text-small cursor-pointer'onClick={()=>{
               localStorage.removeItem("token")
               navigate("/login")
             }}>LogOut</h1>
+            </div>
         </div>
         <div className="content">
             {props.children}
