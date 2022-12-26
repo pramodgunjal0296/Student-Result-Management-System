902e304a58c1b9ded86f8d9cee2eed82a710ea02
pramodgunjal1996 <pramodgunjal1996@gmail.com>
2022-12-26 14:52:25 +0530

    Complet project for deploy

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
-const cors = require('cors')
-const express = require('express')
-const app = express()
-app.use(cors())
-const dotenv=require('dotenv')
+const cors = require("cors");
+const express = require("express");
+const app = express();
+app.use(cors());
+const dotenv = require("dotenv");
 dotenv.config();
-const dbConfig=require('./config/dbConfig')
-dbConfig()
-app.use( (req, res, next) => {
-    console.log("url : "+req.url)
-    next();
+const dbConfig = require("./config/dbConfig");
+dbConfig();
+app.use((req, res, next) => {
+  console.log("url : " + req.url);
+  next();
 });
 app.use(express.json());
 const employeeRoute = require("./routes/employeeRoute");
 const studentRoute = require("./routes/studentRoute");
 const resultsRoute = require("./routes/resultsRoute");
 
-app.use("/api/employee/",employeeRoute);
-app.use("/api/students/",studentRoute);
-app.use("/api/results/",resultsRoute);
+app.use("/api/employee/", employeeRoute);
+app.use("/api/students/", studentRoute);
+app.use("/api/results/", resultsRoute);
+const path = require("path");
 
+const port = process.env.PORT || 9898;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "client/build/index.html"));
+  });
+}
 
-const port = process.env.PORT || 9898
-
-app.listen(port, () => console.log(`Node JS app listening on port ${port}!`))
-
+app.listen(port, () => console.log(`Node JS app listening on port ${port}!`));
